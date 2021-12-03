badf9427e6a815ce0f61a3b0705e9a5fb69fe47f
revathypr <revathypr75@gmail.com>
2021-12-03 14:28:03 +0530

    Dark Mode updated

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var icon=document.getElementById("icon");
+icon.onclick =function(){
+    document.body.classList.toggle("dark_theme")
+    console.log("darkomode");
+
+}
+
+// function dark_mode(){
+//     console.log("darkomode");
+//     element.classList.add("mystyle");
+
+// }
