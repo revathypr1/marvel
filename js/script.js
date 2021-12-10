17240bbc970324209f17b3cf8b96f95ffb398d44
revathypr <revathypr75@gmail.com>
2021-12-10 14:02:05 +0530

    responsive & slider updated

--- js/script.js
+++ js/script.js
@@ -1,12 +1,57 @@
 var icon=document.getElementById("icon");
+
 icon.onclick =function(){
     document.body.classList.toggle("dark_theme")
     console.log("darkomode");
 
 }
 
-// function dark_mode(){
-//     console.log("darkomode");
-//     element.classList.add("mystyle");
+let menuicon = document.getElementById("menu-icon");
+let menu = document.getElementById("mobile-menu");
+let closeto = document.querySelector(".close");
+let overlayto = document.querySelector(".overlay");
+let container = document.getElementById("container");
 
-// }
+menuicon.addEventListener("click", function () {
+    menu.classList.toggle("active");
+    closeto.classList.toggle("active");
+    overlayto.classList.toggle("active");
+    container.classList.toggle("active");
+});
+closeto.addEventListener("click", function () {
+    menu.classList.toggle("active");
+    closeto.classList.toggle("active");
+    overlayto.classList.toggle("active");
+    container.classList.toggle("active");
+
+});
+overlayto.addEventListener("click", function () {
+    menu.classList.toggle("active");
+    closeto.classList.toggle("active");
+    overlayto.classList.toggle("active");
+    container.classList.toggle("active");
+
+});
+
+$('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    nav: true,
+    margin: 10,
+});
+
+
+
+
+    
+
+
+
+
+
+
+
+
